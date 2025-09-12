aa20f650c0bd1c9d23f1dcac1b82e937e542f13f
Kakuren <klong8257@gmail.com>
2025-09-12 16:14:06 +0700

    delete manufacturer_page

--- src/component/tast_bar.tsx
+++ src/component/tast_bar.tsx
@@ -7,7 +7,6 @@ export default function NavbarTabs({ page = "list" }: NavbarTabsProps) {
   const tabs = [
     { id: "list", label: "List of medicines" ,link:"/"},
     { id: "storage", label: "Drug storage" ,link:"/storage"},
-    { id: "manufacturer", label: "Manufacturer" ,link:"/manufacturer"},
     { id: "prescription", label: "Prescription" ,link:"/prescription"},
   ];
   return (
